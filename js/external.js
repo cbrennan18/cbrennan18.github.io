c9d753bd98d5bfcd33d49893b684584995595cc6
Ciarán Brennan <ciaranbrennan18@gmail.com>
2019-10-30 01:27:36 +0000

    first cv commit
    added a home.html page with the first resume section completed. added links and javascript for navigation. changed navbar to fixed. moved footer icons to navbar for the time being.

--- js/external.js
+++ js/external.js
@@ -1,31 +1,32 @@
-// Set the date we're counting down to
-const countDownDate = new Date("Nov 1, 2019 00:00:00").getTime();
+function calculateTime() {
+    // Set the date we're counting down to
+    const countDownDate = new Date("Dec 1, 2019 00:00:00").getTime();
 
-// Update the count down every 1 second
-const x = setInterval(function () {
+    // Update the count down every 1 second
+    const x = setInterval(function () {
 
-    // Get todays date and time
-    const now = new Date().getTime();
+        // Get todays date and time
+        const now = new Date().getTime();
 
-    // Find the distance between now an the count down date
-    const distance = countDownDate - now;
+        // Find the distance between now an the count down date
+        const distance = countDownDate - now;
 
-    // Time calculations for days, hours, minutes and seconds
-    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        // Time calculations for days, hours, minutes and seconds
+        const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    // Display the result in an element with id="demo"
-    document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-        + minutes + "m " + seconds + "s ";
+        // Display the result in an element with id="demo"
+        document.getElementById("demo").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
 
-    // If the count down is finished, write some text
-    if (distance < 0) {
-        clearInterval(x);
-        document.getElementById("demo").innerHTML = "EXPIRED";
-    }
-}, 1000);
+        // If the count down is finished, write some text
+        if (distance < 0) {
+            clearInterval(x);
+            document.getElementById("demo").innerHTML = "EXPIRED";
+        }
+    }, 1000);
+}
 
 function includeHTML() {
     var z, i, elmnt, file, xhttp;
@@ -54,3 +55,30 @@ function includeHTML() {
         }
     }
 }
+
+function hrefSoftScrollClick() {
+    if (/home/.test(window.location.href)) {
+        softScrollClick();
+        return false;
+    }
+    else {
+        window.location.href = "./home.html#scroll1"
+        softScrollClick();
+        return false;
+    }
+}
+
+function softScrollClick() {
+    $('html, body').animate({
+        scrollTop: $("#scroll1").offset().top
+    }, 2000);
+    softScroll();
+}
+function softScroll() {
+    $("#hero-image").css("opacity", 1 - $(window).scrollTop() / ($('#hero-image').height()));
+}
+$(document).ready(function(){
+    $(window).scroll(function(){
+        softScroll();
+    });
+});
